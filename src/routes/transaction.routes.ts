2484d222b92f324fee6561a4c09d4393c1cee7f2
Tami Ribeiro <tamiribeiros@hotmail.com>
2020-11-28 02:04:32 -0300

    feature: listando transações e saldo

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,26 +1,36 @@
 import { Router } from 'express';
 import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
 const transactionsRepository = new TransactionsRepository();
 
-// eslint-disable-next-line consistent-return
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({ transactions, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
 
 transactionRouter.post('/', (request, response) => {
-  const { title, value, type } = request.body;
+  try {
+    const { title, value, type } = request.body;
 
-  const transaction = transactionsRepository.create(title, value, type);
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
 
-  return response.json(transaction);
+    const transaction = createTransaction.execute({ title, value, type });
+
+    return response.json(transaction);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
 });
 
 export default transactionRouter;
